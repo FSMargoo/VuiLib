7d2802f05027a6032c7dd40a1018cb1e0af42bce
GFigure <margoo@foxmail.com>
2022-12-02 12:35:17 +0800

    Added files

---
+++ include/debug/vdebug.h
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <debugapi.h>
+#include <stdio.h>
+
+namespace vdbg {
+#ifndef _v_debug_string_length
+#define _v_debug_string_length 1024
+#endif
+
+	void _v_debug_printf(const char* format, _In_z_ _Printf_format_string_ char const* const parameter, ...);
+	void _v_debug_printf(const char* string);
+}
---
+++ src/debug/vdebug.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "../../include/debug/vdebug.h"
+
+namespace vdbg {
+	void _v_debug_printf(const char* format, _In_z_ _Printf_format_string_ char const* const parameter, ...) {
+#ifdef _DEBUG
+		char print_string[_v_debug_string_length];
+		sprintf(print_string, format, parameter);
+
+		OutputDebugStringA(print_string);
+#else
+
+#endif
+	}
+	void _v_debug_printf(const char* string) {
+#ifdef _DEBUG
+		OutputDebugStringA(string);
+#else
+
+#endif
+	}
+}
