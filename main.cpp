831971e3477d266ac31ff5b766cce4ef31fd358c
M argoo <1683691371@qq.com>
2024-06-08 12:23:46 +0800

    Added event lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vtest/vException.h>
+
+void B() {
+	printf("A");
+}
+
+void C() {
+	Core::VSignal<> A;
+	A.Connect(B);
+	A.Connect(B);
+	A.Connect(B);
+	A.Connect(B);
+	A.Connect(B);
+	A.Emit();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+	std::function<void()> Function;
+	Function = B;
+	std::function<void()> Function1;
+	Function1 = C;
+	auto _uuid = reinterpret_cast<long long>(Function.template target<void>());
+	auto _uuid1 = reinterpret_cast<long long>(Function1.template target<void>());
+
+	printf("%lld\n%lld", _uuid, _uuid1);
+
     return 0;
 }
